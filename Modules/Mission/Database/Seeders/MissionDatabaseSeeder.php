bfd7569f7069b3bf750f26c3a479d51a670e9170
Daniyal <daniyalx77@gmail.com>
2024-06-23 21:25:48 +0330

    add module mission

---
+++ Modules/Mission/Database/Seeders/MissionDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Mission\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class MissionDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
